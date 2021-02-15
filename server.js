66ee176dae2e437b8e40ed8e97753e3e2f48ac45
hunbalSiddiqui <hunbal.dha@gmail.com>
2021-02-16 04:07:59 +0500

    MHS 1.0.2 Mongoose connected, saving data required..

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
+const mongoose = require('mongoose')
 const app = require('./app')
+
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser : true,
+    useCreateIndex : true,
+    useFindAndModify : false
+})
+.then((connection)=>{
+    console.log(connection)
+    console.log("DB ATLAS CONNECTION SUCCESSFUL!")
+})
+.catch((err)=>{
+    console.log("connection can be done right now.",err)
+})
 //Port
 const port = 8000;
 //Starting point of a server
